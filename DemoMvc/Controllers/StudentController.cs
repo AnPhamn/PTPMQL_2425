063cc43d69866fbf7f3f5ed8d718e803ab0b4fbf
AnPhamn <phamthu141219@gmail.com>
2024-08-20 13:49:19 +0700

    Update_Code

---
+++ DemoMvc/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+namespace DEMOMVC.Controllers;
+
+
+public class StudentController : Controller
+{
+   public IActionResult Index()
+    {
+        return View();
+    }
+
+    private IActionResult View()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public IActionResult Index(string MaSinhVien, string HoTen)
+    {
+        string strResult = "Xin chao " + MaSinhVien + "-" + HoTen;
+        ViewBag.Info = strResult;
+        return View();
+    }
+}
