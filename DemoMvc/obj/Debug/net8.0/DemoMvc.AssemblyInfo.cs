0ac5707bf6b7b8fb62bec32be8c357fa22b954a8
AnPhamn <phamthu141219@gmail.com>
2024-08-21 15:37:38 +0700

    Update_Code

--- DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063cc43d69866fbf7f3f5ed8d718e803ab0b4fbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0071a60c39c9056b6dfbfa5c009d704d1780dfae")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
